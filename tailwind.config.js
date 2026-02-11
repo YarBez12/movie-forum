6a2bd7c27c42a21ae54669456b5d664d56e4eed7
YarBez12 <yaroslav.bezvesilnyi@nure.ua>
2026-02-11 15:44:50 +0000

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/**/*.hbs",
+    "./public/**/*.js",
+    "./WHTML/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
